a1c7a6c2327389e903126f9c7a30c68624f1fc30
Radin <radin@rngfx.com>
2025-05-02 19:43:49 -0700

    finally got tailwind working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/electron-vite.animate.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +28,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="absolute top-0 left-0 w-full h-[260px] z-0 pointer-events-none">
+        <div className="absolute inset-0 bg-gradient-to-tr from-sky-400/20 via-fuchsia-400/10 to-indigo-400/20 blur-2xl opacity-70 animate-gradient-shift-slow" />
+        <div className="absolute left-1/2 top-0 -translate-x-1/2 w-[60vw] h-[180px] rounded-full bg-gradient-to-r from-sky-400/30 via-purple-400/20 to-pink-400/20 blur-3xl opacity-60 animate-gradient-rotate" />
+      </div>{" "}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
